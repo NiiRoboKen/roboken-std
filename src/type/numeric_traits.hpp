29fe55086bdbe684076b7344dc37a74d2d5d1a35
Netetra <netetra07@gmail.com>
2024-12-23 14:07:00 +0900

    feat: 符号付き整数型を示すtraits

---
+++ src/type/numeric_traits.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "numeric.hpp"
+#include "traits.hpp"
+
+template <typename T>
+constexpr bool is_int_t = false
+  || is_type_eq<T, int8_t>
+  || is_type_eq<T, int16_t>
+#if !defined(CANT_USE_32BITS_INTEGER_TYPE)
+  || is_type_eq<T, int32_t>
+#endif
+#if !defined(CANT_USE_64BITS_INTEGER_TYPE)
+  || is_type_eq<T, int64_t>
+#endif
+  ;
+
