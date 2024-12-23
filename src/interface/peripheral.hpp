dbebb99305d02531f0dfe8e6f59679440535cabe
Netetra <netetra07@gmail.com>
2024-12-23 19:00:14 +0900

    refactor: format

--- src/interface/peripheral.hpp
+++ src/interface/peripheral.hpp
@@ -1,9 +1,8 @@
 #pragma once
 
-#include "../util.hpp"
 #include "../type/numeric.hpp"
 #include "../type/numeric_traits.hpp"
-
+#include "../util.hpp"
 
 interface Motor {
   // NOTE: ロボコンでは分解能は16bitで十分と思われる
@@ -11,10 +10,11 @@ interface Motor {
   virtual void ccw(uint16_t speed) = 0;
   virtual void run(int32_t speed) {
     if (speed >= 0) {
-      this->cw(speed <= TypeInfo<uint16_t>::MAX ? speed : TypeInfo<uint16_t>::MAX);
-    }
-    else {
-      this->ccw(-speed >= TypeInfo<uint16_t>::MIN ? speed : TypeInfo<uint16_t>::MAX);
+      this->cw(speed <= TypeInfo<uint16_t>::MAX ? speed
+                                                : TypeInfo<uint16_t>::MAX);
+    } else {
+      this->ccw(-speed >= TypeInfo<uint16_t>::MIN ? speed
+                                                  : TypeInfo<uint16_t>::MAX);
     }
   }
 };
