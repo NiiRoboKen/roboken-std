ac50440edf84e187879392b08481298e8e55e24e
Sootta <147506050+Sootta@users.noreply.github.com>
2025-01-28 14:12:11 +0900

    feat: moveを追加

--- src/robotics/omuni.hpp
+++ src/robotics/omuni.hpp
@@ -3,13 +3,18 @@
 #include "../math/functions.hpp"
 #include "../interface/peripheral.hpp"
 
-template<typename T, uint8_t DEGREE>
+template<typename T>
 class Omuni {
     using U = float_t<T>;
     public:
-    Omuni(Motor *motor) {
+    Omuni(Motor *motor, U radian) {
         this->motor = motor;
+        this->degree = radian;
+    }
+    void move(uint16_t x, uint16_t y) {
+        this->motor->run(cos<U, 25>(this->radian) * x + sin<U, 25>(this->radian) * y);
     }
     private:
     Motor *motor;
+    U radian;
 };
